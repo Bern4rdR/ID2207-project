0cb948c0e8dacc99592dba99a036da1f90bc9571
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-10-24 14:30:09 +0200

    login andded with tests. Tests are passing currently

---
+++ login/login_manager.py
@@ -0,0 +1,18 @@
+
+class LoginManager:
+    _users = {}
+    def __init__(self, filename='users.txt'):
+        self._filename = filename
+        with open(filename, 'r') as f:
+            for line in f.readlines():
+                user = line.split(",")[0]
+                password = line.split(",")[1]
+                self._users[user] = password
+
+    def login(self, username='', password=''):
+        if username == '' or password == '':
+            return False
+        if username in self._users.keys():
+            if self._users[username] == password:
+                return True
+        return False
--- login/login_tests.py
+++ login/login_tests.py
@@ -1,7 +1,39 @@
-def test_login():
-    # Empty fields
-    # Invalid credentials
-    # Valid credentials
-    # Role-based response
-    pass
+from login.login_manager import LoginManager
 
+def test_login_manager_init():
+    lm = LoginManager("users.txt")
+    assert lm != None
+
+def test_login_manager_no_file():
+    lm = LoginManager()
+    assert lm !=None
+
+def test_login_empty():
+    lm = LoginManager()
+    success = lm.login()
+    assert not success
+
+def test_login_correct():
+    lm = LoginManager()
+    success = lm.login("vivienne", "password")
+    assert not success
+
+def test_login_wrong_username():
+    lm = LoginManager()
+    success = lm.login("vivie", "password")
+    assert not success
+
+def test_login_no_username():
+    lm = LoginManager()
+    success = lm.login("", "password")
+    assert not success
+
+def test_login_no_password():
+    lm = LoginManager()
+    success = lm.login("vivienne", "")
+    assert not success
+
+def test_login_wrong_password():
+    lm = LoginManager()
+    success = lm.login("vivienne", "pass")
+    assert not success
