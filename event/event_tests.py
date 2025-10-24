4069c8d4a8fad712c7f56f312bf8672019bbe25e
Bern4rdR <rumarbernard@gmail.com>
2025-10-24 15:19:07 +0200

    add tests for event request

--- event/event_tests.py
+++ event/event_tests.py
@@ -1,33 +1,97 @@
-def test_initiate_request():
-    # Invalid credentials
-    # Valid credentials
+import Request
+from Request import EventRequest  # inherits from Request abstract class
+import Status
+import datetime
+
+
+SAMPLE_EVENT_REQUEST = EventRequest(
+    type="Conference",
+    budget=100,
+    dates=[datetime.datetime.now() + datetime.timedelta(days=1)],
+    preferences=["Quiet", "Enough room for 10 people"],
+)
+SAMPLE_EVENTS = [SAMPLE_EVENT_REQUEST]
+
+
+def test_initiate_event_request():
+    # TODO: Invalid credentials
+    # TODO: Valid credentials
+
+    request = EventRequest(
+        type="Conference",
+        budget=100,
+        dates=[datetime.datetime.now() + datetime.timedelta(days=1)],
+        preferences=["Quiet", "Enough room for 10 people"],
+    )
+
     # Required fields
-    # Date validation
-    pass
+    assert request.id is not None
+    assert request.status == Status.Ongoing
+    assert request.type == "Conference"
+    assert request.budget == 100
+    assert request.feedback == []
+    assert request.dates != []
+    assert request.preferences == ["Quiet", "Enough room for 10 people"]
+    # TODO: link to event details
+
+    # Fields validation
+    assert not any([d < datetime.datetime.now() for d in request.dates])
+    assert request.budget >= 0
 
 
 def test_view_event_request():
-    # Request found
     # Request not found
-    # Access control
-    pass
+    assert Request.get(1234567890, SAMPLE_EVENTS) is None
+
+    # Request found
+    assert (
+        Request.get(SAMPLE_EVENT_REQUEST.id, SAMPLE_EVENTS).id
+        == SAMPLE_EVENT_REQUEST.id
+    )
+    # View event details
+    assert len(str(SAMPLE_EVENT_REQUEST)) > 0
+
+    # TODO: Access control
 
 
 def test_customer_service_desicion():
+    # TODO: Invalid credentials
+    # TODO: Valid credentials
+
     # Approval
+    SAMPLE_EVENT_REQUEST.approve()
+    assert SAMPLE_EVENT_REQUEST.status == Status.Approved
     # Rejection
-    # State validation
-    pass
+    SAMPLE_EVENT_REQUEST.reject(reason="Not enough venue capacity")
+    assert SAMPLE_EVENT_REQUEST.status == Status.Rejected
 
 
 def test_financial_manager_feedback():
+    # TODO: Invalid credentials
+    # TODO: Valid credentials
+
+    # Feedback
+    SAMPLE_EVENT_REQUEST.addFeedback("Not financially viable")
+    SAMPLE_EVENT_REQUEST.addFeedback("Budget not enough for venue")
+    assert SAMPLE_EVENT_REQUEST.feedback == [
+        "Not financially viable",
+        "Budget not enough for venue",
+    ]
     # Approval
+    SAMPLE_EVENT_REQUEST.approve()
+    assert SAMPLE_EVENT_REQUEST.status == Status.Approved
     # Rejection
-    # Budget validation
-    pass
+    SAMPLE_EVENT_REQUEST.reject(reason="Not enough venue capacity")
+    assert SAMPLE_EVENT_REQUEST.status == Status.Rejected
 
 
 def test_admin_manager_desicion():
+    # TODO: Invalid credentials
+    # TODO: Valid credentials
+
     # Final Approval
+    SAMPLE_EVENT_REQUEST.approve()
+    assert SAMPLE_EVENT_REQUEST.status == Status.Approved
     # Final Rejection
-    pass
+    SAMPLE_EVENT_REQUEST.close()
+    assert SAMPLE_EVENT_REQUEST.status == Status.Closed
