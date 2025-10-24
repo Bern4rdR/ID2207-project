21dd78e8100c3ba67038d052c79c5366fd26b25f
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-10-24 14:11:46 +0200

    added new modules and start_app

---
+++ event/event_tests.py
@@ -0,0 +1,33 @@
+def test_initiate_request():
+    # Invalid credentials
+    # Valid credentials
+    # Required fields
+    # Date validation
+    pass
+
+
+def test_view_event_request():
+    # Request found
+    # Request not found
+    # Access control
+    pass
+
+
+def test_customer_service_desicion():
+    # Approval
+    # Rejection
+    # State validation
+    pass
+
+
+def test_financial_manager_feedback():
+    # Approval
+    # Rejection
+    # Budget validation
+    pass
+
+
+def test_admin_manager_desicion():
+    # Final Approval
+    # Final Rejection
+    pass
---
+++ login/login_tests.py
@@ -0,0 +1,7 @@
+def test_login():
+    # Empty fields
+    # Invalid credentials
+    # Valid credentials
+    # Role-based response
+    pass
+
